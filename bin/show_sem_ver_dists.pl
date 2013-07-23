8513d1f92d4e06e2884976b443fc1b09fe43d53e
Ivan Bessarabov <ivan@bessarabov.ru>
2013-07-23 20:20:18 +0400

    Initial commit

---
+++ bin/show_sem_ver_dists.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+=encoding UTF-8
+=cut
+
+=head1 DESCRIPTION
+
+=cut
+
+# common modules
+use strict;
+use warnings FATAL => 'all';
+use 5.010;
+use utf8;
+use open qw(:std :utf8);
+
+use DDP;
+use Carp;
+use lib::abs qw(
+    ./lib
+);
+
+# global vars
+
+# subs
+
+# main
+sub main {
+
+    say '#END';
+}
+
+main();
+__END__
